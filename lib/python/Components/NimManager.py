5d4afc980de1df3181b83715fb126b13a2d9fb70
norhap <alvaro@elfogondesegovia.com>
2019-03-18 09:32:45 +0100

    Update NimManager.py

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -1363,24 +1363,24 @@ def InitNimManager(nimmgr, update_slots = []):
 			tmp = ConfigSelection(lnb_choices, lnb_choices_default)
 			tmp.slot_id = x
 			tmp.lnb_id = lnb
-			tmp.addNotifier(configLOFChanged, initial_call=False)
+			tmp.addNotifier(configLOFChanged)
 			section.lof = tmp
 
 	def scpcSearchRangeChanged(configElement):
 		fe_id = configElement.fe_id
 		slot_id = configElement.slot_id
 		if os.path.exists("/proc/stb/frontend/%d/use_scpc_optimized_search_range" % fe_id):
-			f = open("/proc/stb/frontend/%d/use_scpc_optimized_search_range" % (fe_id), "w")
-			f.write(configElement.value)
-			f.close()
+			open("/proc/stb/frontend/%d/use_scpc_optimized_search_range" % (fe_id), "w").write(configElement.value)
+			
+			
 
 	def toneAmplitudeChanged(configElement):
 		fe_id = configElement.fe_id
 		slot_id = configElement.slot_id
 		if os.path.exists("/proc/stb/frontend/%d/tone_amplitude" % fe_id):
-			f = open("/proc/stb/frontend/%d/tone_amplitude" % fe_id, "w")
-			f.write(configElement.value)
-			f.close()
+			open("/proc/stb/frontend/%d/tone_amplitude" %(fe_id), "w").write(configElement.value)
+			
+			
 
 	def t2miRawModeChanged(configElement):
 		slot = configElement.slot
@@ -1442,7 +1442,7 @@ def InitNimManager(nimmgr, update_slots = []):
 			tmp.rotorposition = ConfigInteger(default=1, limits=(1, 255))
 			lnb = ConfigSelection(advanced_lnb_choices, "0")
 			lnb.slot_id = slot_id
-			lnb.addNotifier(configLNBChanged, initial_call=False)
+			lnb.addNotifier(configLNBChanged)
 			tmp.lnb = lnb
 			nim.advanced.sat[x[0]] = tmp
 		for x in range(3601, 3607):
@@ -1455,7 +1455,7 @@ def InitNimManager(nimmgr, update_slots = []):
 			lnbnum = 65 + x - 3601
 			lnb = ConfigSelection([("0", _("not configured")), (str(lnbnum), "LNB %d"%(lnbnum))], "0")
 			lnb.slot_id = slot_id
-			lnb.addNotifier(configLNBChanged, initial_call=False)
+			lnb.addNotifier(configLNBChanged)
 			tmp.lnb = lnb
 			nim.advanced.sat[x] = tmp
 
@@ -1503,6 +1503,7 @@ def InitNimManager(nimmgr, update_slots = []):
 
 	def tunerTypeChanged(nimmgr, configElement, initial=False):
 		fe_id = configElement.fe_id
+		print "[InitNimManager] tunerTypeChanged: setFrontendType %s" % nimmgr.nim_slots[fe_id].getType()																								   
 		eDVBResourceManager.getInstance().setFrontendType(nimmgr.nim_slots[fe_id].frontend_id, nimmgr.nim_slots[fe_id].getType())
 		try:
 			raw_channel = eDVBResourceManager.getInstance().allocateRawChannel(fe_id)
@@ -1567,6 +1568,7 @@ def InitNimManager(nimmgr, update_slots = []):
 				createConfig(nim, slot)
 			else:
 				print "[InitNimManager] disable hotswitchable tuner"
+				eDVBResourceManager.getInstance().setFrontendType(nimmgr.nim_slots[fe_id].frontend_id, "UNDEFINED")																									   
 				nim.configMode.value = nim.configMode.default = "nothing"
 
 	def createConfig(nim, slot):
